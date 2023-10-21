d0fdc19e21ac8b94d89b95b45adb559b1eb67058
sofffja <sofi.efron@gmail.com>
2023-10-21 17:26:20 -0300

    agujaaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
           11/11/23
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center  lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center">
           <a
-            className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="flex place-items-center gap-2 p-8 "
             href="https://soundcloud.com/aguja6167756a61"
             target="_blank"
             rel="noopener noreferrer"
@@ -18,7 +18,7 @@ export default function Home() {
             soundcloud
           </a>
           <a
-            className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="flex place-items-center gap-2 p-8 "
             href="https://www.instagram.com/aguja6167756a61/"
             target="_blank"
             rel="noopener noreferrer"
@@ -26,7 +26,7 @@ export default function Home() {
             instagram
           </a>
           <a
-            className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="flex place-items-center gap-2 p-8 "
             href="https://unun.link"
             target="_blank"
             rel="noopener noreferrer"
