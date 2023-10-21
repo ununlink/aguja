abc193307136557d0382cd382113090addf615a2
sofffja <sofi.efron@gmail.com>
2023-10-21 17:56:07 -0300

    no dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+        <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 backdrop-blur-2xl  ">
           11/11/23
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center">
