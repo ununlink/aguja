8f5dcbd5022319a75118427ab37aa0b688024ccb
astrosuka <yegresvoselok@gmail.com>
2023-11-06 17:31:15 -0300

    preorder

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,19 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+       
         <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 backdrop-blur-2xl  ">
-          11/11/23
+          11/11/23 
         </p>
+          <a
+            className="flex w-full justify-center"
+            href="https://unun.bandcamp.com/album/un002-m-sica-electr-nica-i"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            preorder 'musica electronica I'
+          </a>
+      
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center">
           <a
             className="flex place-items-center gap-2 p-8 "
