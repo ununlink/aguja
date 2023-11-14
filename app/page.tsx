e7bcc84823e3f4da626442c00306fd40886a4c6f
astrosuka <yegresvoselok@gmail.com>
2023-11-14 00:21:41 -0300

    como linktree pero mas lindo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
        
         <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8">
-          🫠
+          🎼 [UN002] M​ú​sica Electr​ó​nica I
         </p>
           <a
             className="flex w-full justify-center text-center"
@@ -15,43 +15,61 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            musica electronica I
+           buy on bandcamp
           </a>
-      
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center">
+
           <a
-            className="flex place-items-center gap-2 p-8 "
+            className="flex w-full justify-center text-center"
+            href="https://unun.link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           stream on unun.link
+          </a>
+
+          <a
+            className="flex w-full justify-center text-center"
             href="https://soundcloud.com/aguja6167756a61"
             target="_blank"
             rel="noopener noreferrer"
           >
-            soundcloud
+           stream on soundcloud
           </a>
+
+          {/* <a
+            className="flex w-full justify-center text-center pt-8"
+            href="https://unun.bandcamp.com/follow_me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           newsletter
+          </a> */}
+
+          <span
+          className="flex w-full justify-center text-center pt-10 text-2xl"
+          >
+            🪡
+          </span>
+          
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center">
           <a
             className="flex place-items-center gap-2 p-8 "
             href="https://www.instagram.com/aguja6167756a61/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            instagram
-          </a>
-          <a
-            className="flex place-items-center gap-2 p-8 "
-            href="https://unun.link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            unun.link
+           instagram
           </a>
 
           <a
             className="flex place-items-center gap-2 p-8 "
-            href="https://unun.bandcamp.com/album/un002-m-sica-electr-nica-igit "
+            href="https://unun.bandcamp.com/follow_me"
             target="_blank"
             rel="noopener noreferrer"
           >
-            bandcamp
+           newsletter
           </a>
+          
         </div>
       </div>
       
