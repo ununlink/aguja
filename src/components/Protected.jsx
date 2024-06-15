f5b283c2ddb3bba821b52a9e3b9d9a045bbdc99b
astrosuka <yegresvoselok@gmail.com>
2024-06-15 18:34:17 -0300

    Add sounds and custom error message to Protected

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Protected = ({ password, children }) => {
   const [enteredPassword, setEnteredPassword] = useState("");
   const [isAuthorized, setIsAuthorized] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const correctSoundRef = useRef(null);
+  const incorrectSoundRef = useRef(null);
+  const errorTimeoutRef = useRef(null);
 
   const handlePasswordChange = (e) => {
     setEnteredPassword(e.target.value);
@@ -11,32 +15,49 @@ const Protected = ({ password, children }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (enteredPassword === password) {
+      correctSoundRef.current.play();
       setIsAuthorized(true);
     } else {
-      alert("Incorrect password");
+      incorrectSoundRef.current.play();
+      setErrorMessage("incorrecto");
+      clearTimeout(errorTimeoutRef.current);
+      errorTimeoutRef.current = setTimeout(() => {
+        setErrorMessage("");
+      }, 2000);
     }
   };
 
-  if (isAuthorized) {
-    return <>{children}</>;
-  }
+  useEffect(() => {
+    return () => {
+      clearTimeout(errorTimeoutRef.current);
+    };
+  }, []);
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit}
-        className="items-centerpy-2 flex py-2 text-xs"
-      >
-        <div className="flex items-center gap-1">
-          <input
-            type="password"
-            value={enteredPassword}
-            onChange={handlePasswordChange}
-            className="w-full"
-          />
-          <button type="submit">→</button>
-        </div>
-      </form>
+      {isAuthorized ? (
+        children
+      ) : (
+        <>
+          <form
+            onSubmit={handleSubmit}
+            className="items-centerpy-2 flex py-2 text-xs"
+          >
+            <div className="flex items-center gap-1">
+              <input
+                type="password"
+                value={enteredPassword}
+                onChange={handlePasswordChange}
+                className="w-full"
+              />
+              <button type="submit">→</button>
+            </div>
+          </form>
+          {errorMessage && <p className="text-xs text-[red]">{errorMessage}</p>}
+        </>
+      )}
+      <audio ref={correctSoundRef} src="/si.mp3" />
+      <audio ref={incorrectSoundRef} src="/no.mp3" />
     </div>
   );
 };
