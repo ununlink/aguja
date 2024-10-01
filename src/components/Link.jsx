e0083ef40e8c50ae157cde924d04dc9eb71580c4
astrosuka <yegresvoselok@gmail.com>
2024-10-01 18:18:24 -0300

    Adjust style

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,11 +1,11 @@
 export default function Link() {
   return (
-    <div className="fixed right-0 top-0 flex gap-1 text-xs">
+    <div className="fixed right-0 top-0 m-2 flex gap-1 rounded-full text-xs hover:bg-[#000]">
       <a
         href="https://unun.bandcamp.com/album/un002-m-sica-electr-nica-i"
         target="_blank"
       >
-        <img src="/me1.gif" alt=":)" className="m-2 size-[50px] cursor-help" />
+        <img src="/me1.gif" alt=":)" className="size-[50px] cursor-help" />
       </a>
     </div>
   );
