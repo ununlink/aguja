1dcde8a49d70900a31d8afd877d8be3db0258eca
sofffja <sofi.efron@gmail.com>
2025-06-10 03:12:48 +0000

    Refactor button styles in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -14,9 +14,9 @@ export default function Player({ embed }) {
     >
       <div
         onClick={() => setPlayerOpen(!playerOpen)}
-        className="pointer-events-auto m-4 flex size-10 cursor-pointer select-none items-center justify-center border border-black bg-[#fff]/50 p-2 text-xl text-[#000] hover:bg-white"
+        className="pointer-events-auto m-4 flex cursor-pointer select-none items-center justify-center rounded-full border px-2 pb-1 text-xl text-white hover:bg-[#f00]"
       >
-        <div>{playerOpen ? <span>x</span> : <span>: )</span>}</div>
+        <div>{playerOpen ? <span>(X)</span> : <span>(~)</span>}</div>
       </div>
       <div className="pointer-events-auto h-[400px] w-full opacity-95">
         <iframe
