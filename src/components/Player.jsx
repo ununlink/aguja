cee5871d339e78fe24e1c27e228cdf3d1fb13183
astrosuka <yegresvoselok@gmail.com>
2025-06-10 16:27:53 -0300

    Use button tag for button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -16,7 +16,7 @@ export default function Player({ embed }) {
         onClick={() => setPlayerOpen(!playerOpen)}
         className="pointer-events-auto m-4 flex cursor-pointer select-none items-center justify-center rounded-full border px-2 pb-1 text-xl text-white hover:bg-[#f00]"
       >
-        <div>{playerOpen ? <span>(X)</span> : <span>(~)</span>}</div>
+        <button>{playerOpen ? <span>(X)</span> : <span>(~)</span>}</button>
       </div>
       <div className="pointer-events-auto h-[400px] w-full opacity-95">
         <iframe
